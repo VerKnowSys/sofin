fea4e5ec305f3378b7d8ffac9ef3d158436d2151
Michał Lipski <tallica@o2.pl>
2013-03-05 02:52:45 +0100

    Set version to 0.37.0.

--- sofin.sh
+++ sofin.sh
@@ -2,7 +2,7 @@
 # @author: Daniel (dmilith) Dettlaff (dmilith@verknowsys.com)
 
 # config settings
-VERSION="0.36.4"
+VERSION="0.37.0"
 # load configuration from sofin.conf
 
 CONF_FILE="/etc/sofin.conf.sh"
