4cc393fd6f3998b246a3a5ee556be469a3c59fdb
Michał Lipski <tallica@o2.pl>
2013-03-05 19:03:39 +0100

    Update shell environment files during sofin-install.

--- sofin.sh
+++ sofin.sh
@@ -2,7 +2,7 @@
 # @author: Daniel (dmilith) Dettlaff (dmilith@verknowsys.com)
 
 # config settings
-VERSION="0.38.1"
+VERSION="0.39.0"
 
 # load configuration from sofin.conf
 CONF_FILE="/etc/sofin.conf.sh"
