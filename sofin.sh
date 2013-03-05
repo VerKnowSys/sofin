803201f0fa21bd7ed9d387060af6d096790d2c31
Michał Lipski <tallica@o2.pl>
2013-03-05 19:17:08 +0100

    Get rid of bash specific code.

--- sofin.sh
+++ sofin.sh
@@ -2,7 +2,7 @@
 # @author: Daniel (dmilith) Dettlaff (dmilith@verknowsys.com)
 
 # config settings
-VERSION="0.39.0"
+VERSION="0.39.1"
 
 # load configuration from sofin.conf
 CONF_FILE="/etc/sofin.conf.sh"
