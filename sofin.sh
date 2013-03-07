7cb9aea89390c77d59dd75d750e9d26050ac80a2
Michał Lipski <tallica@o2.pl>
2013-03-07 19:28:25 +0100

    Updated Sofin version to 0.39.3.

--- sofin.sh
+++ sofin.sh
@@ -2,7 +2,7 @@
 # @author: Daniel (dmilith) Dettlaff (dmilith@verknowsys.com)
 
 # config settings
-VERSION="0.39.2"
+VERSION="0.39.3"
 
 # load configuration from sofin.conf
 CONF_FILE="/etc/sofin.conf.sh"
