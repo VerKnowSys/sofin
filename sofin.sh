90b3d915d842ce777164a1defb125fad6e2393dc
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-05-24 18:28:21 +0200

    Rpath fight. Stage 2.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- sofin.sh
+++ sofin.sh
@@ -2,7 +2,7 @@
 # @author: Daniel (dmilith) Dettlaff (dmilith@verknowsys.com)
 
 # config settings
-readonly VERSION="0.48.0"
+readonly VERSION="0.47.0"
 
 # load configuration from sofin.conf
 readonly CONF_FILE="/etc/sofin.conf.sh"
@@ -692,7 +692,7 @@ for application in ${APPLICATIONS}; do
                 # export LD_LIBRARY_PATH="${PREFIX}/lib:${PREFIX}/libexec:/usr/lib:/lib"
                 export CFLAGS="-I${PREFIX}/include ${APP_COMPILER_ARGS} ${DEFAULT_COMPILER_FLAGS}"
                 export CXXFLAGS="-I${PREFIX}/include ${APP_COMPILER_ARGS} ${DEFAULT_COMPILER_FLAGS}"
-                export LDFLAGS="-L${PREFIX}/lib ${APP_LINKER_ARGS} ${DEFAULT_LDFLAGS} -Wl,--enable-new-dtags" # -rpath=${PREFIX}/lib,
+                export LDFLAGS="-L${PREFIX}/lib ${APP_LINKER_ARGS} ${DEFAULT_LDFLAGS} -Wl,-rpath=../../${APP_NAME}/lib,--enable-new-dtags"
 
                 if [ "${SYSTEM_NAME}" = "Darwin" ]; then
                     export PATH="${PATH}:/opt/X11/bin" # NOTE: requires XQuartz installed!
