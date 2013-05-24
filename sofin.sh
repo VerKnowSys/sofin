74b6ba7d93608881f32a9d5ac2c7b77a9e23c6ae
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-05-24 18:44:56 +0200

    Continued experiments. Stage 4.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- sofin.sh
+++ sofin.sh
@@ -692,7 +692,8 @@ for application in ${APPLICATIONS}; do
                 # export LD_LIBRARY_PATH="${PREFIX}/lib:${PREFIX}/libexec:/usr/lib:/lib"
                 export CFLAGS="-I${PREFIX}/include ${APP_COMPILER_ARGS} ${DEFAULT_COMPILER_FLAGS}"
                 export CXXFLAGS="-I${PREFIX}/include ${APP_COMPILER_ARGS} ${DEFAULT_COMPILER_FLAGS}"
-                export LDFLAGS="-L${PREFIX}/lib ${APP_LINKER_ARGS} ${DEFAULT_LDFLAGS} -Wl,-rpath=\$ORIGIN/../lib,--enable-new-dtags"
+                ORIGIN_SRC='$ORIGIN/../lib'
+                export LDFLAGS="-L${PREFIX}/lib ${APP_LINKER_ARGS} ${DEFAULT_LDFLAGS} -Wl,-rpath=${ORIGIN_SRC},--enable-new-dtags"
 
                 if [ "${SYSTEM_NAME}" = "Darwin" ]; then
                     export PATH="${PATH}:/opt/X11/bin" # NOTE: requires XQuartz installed!
