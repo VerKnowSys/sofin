6b0544df8bc2e5d6c13a50915121581112f0c512
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-06-24 14:04:43 +0200

    Bumped version after fix in launcher.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- sofin.sh
+++ sofin.sh
@@ -2,7 +2,7 @@
 # @author: Daniel (dmilith) Dettlaff (dmilith@verknowsys.com)
 
 # config settings
-readonly VERSION="0.48.11"
+readonly VERSION="0.48.12"
 
 # load configuration from sofin.conf
 readonly CONF_FILE="/etc/sofin.conf.sh"
