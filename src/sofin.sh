28642bcc4d8c9c635e7deddb5c9cbb30e85791ca
Daniel (dmilith) Dettlaff <dmilith@me.com>
2014-02-07 13:29:50 +0100

    Added UNZIP_BIN value.

--- src/sofin.sh
+++ src/sofin.sh
@@ -2,7 +2,7 @@
 # @author: Daniel (dmilith) Dettlaff (dmilith@verknowsys.com)
 
 # config settings
-readonly VERSION="0.58.9"
+readonly VERSION="0.58.10"
 
 # load configuration from sofin.conf
 readonly CONF_FILE="/etc/sofin.conf.sh"
