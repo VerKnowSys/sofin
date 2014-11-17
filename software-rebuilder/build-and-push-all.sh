6ad0a201a44247fc95b32fa21c023c644a54d282
Daniel (dmilith) Dettlaff <dmilith@me.com>
2014-11-17 19:08:28 +0100

    Basic script for software rebuilder.

---
+++ software-rebuilder/build-and-push-all.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+
+chmod 600 ~/.ssh/id_rsa
+
+. /etc/sofin.conf.sh
+
+set -e
+
+note "Checking remote machine connection (shouldn't take more than a second).."
+ssh sofin@verknowsys.com "uname -a"
+
+
+for software in $(cat software.list); do
+    note "Processing software: ${software}"
+    USE_BINBUILD=false sofin get ${software}
+    sofin push ${software}
+    sofin remove ${software}
+done
