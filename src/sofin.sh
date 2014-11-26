d2d5ca11c5ecd5d0a9181bb0fc5e3a3d43b67492
Daniel (dmilith) Dettlaff <dmilith@me.com>
2014-11-26 23:09:39 +0100

    Use new home for normal user: /User

--- src/sofin.sh
+++ src/sofin.sh
@@ -728,7 +728,7 @@ for application in ${APPLICATIONS}; do
 
             # note "Preparing application: ${APP_NAME}${APP_POSTFIX} (${APP_FULL_NAME} v${APP_VERSION})"
             export PREFIX="${SOFTWARE_DIR}${APP_NAME}${APP_POSTFIX}"
-            export SOFTWARE_DATA_DIR="/SoftwareData/${APP_NAME}${APP_POSTFIX}"
+            export SOFTWARE_DATA_DIR="/User/SoftwareData/${APP_NAME}${APP_POSTFIX}"
             if [ "${USERNAME}" = "root" ]; then
                 export SOFTWARE_DATA_DIR="${SYSTEM_HOME_DIR}SoftwareData/${APP_NAME}${APP_POSTFIX}"
             fi
