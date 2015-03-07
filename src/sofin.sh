084764aacb61521cfd7e4f48b30860c5fe6cb7dd
Daniel (dmilith) Dettlaff <dmilith@me.com>
2015-03-07 14:12:55 +0100

    Version bump.

--- src/sofin.sh
+++ src/sofin.sh
@@ -2,7 +2,7 @@
 # @author: Daniel (dmilith) Dettlaff (dmilith at me dot com)
 
 # config settings
-readonly VERSION="0.76.4"
+readonly VERSION="0.76.5"
 
 # load configuration from sofin.conf
 readonly CONF_FILE="/etc/sofin.conf.sh"
