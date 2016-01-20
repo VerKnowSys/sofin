95dc8729dd36cfca6e880d5e0c2fc971c3db29fd
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-01-20 23:54:55 +0100

    Moved build host setup to ServeD-OS repository.

--- software-rebuilder/build-and-push-all.sh
+++ software-rebuilder/build-and-push-all.sh
@@ -9,17 +9,6 @@ chmod 600 ~/.ssh/id_rsa
 note "Checking remote machine connection (shouldn't take more than a second).."
 ssh sofin@verknowsys.com "uname -a"
 
-if [ "${SYSTEM_NAME}" = "FreeBSD" ]; then
-    ${UNAME_BIN} -a | ${GREP_BIN} "HBSD" >/dev/null 2>&1
-    if [ "$?" = "0" ]; then
-        note "Setting pageexec and mprotect to 1 for build purposes"
-        sysctl hardening.pax.pageexec.status=1
-        sysctl hardening.pax.mprotect.status=1
-        note "Loading Dtrace"
-        kldload dtraceall
-    fi
-fi
-
 for software in $(cat software.list); do
     note "________________________________"
     note "Processing software: ${software}"
