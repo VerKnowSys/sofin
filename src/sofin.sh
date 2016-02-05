1c657000e5e8f3dc559faea2dfae6ade74c25c75
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-02-05 14:31:42 +0100

    fix: Linux shell is designed to be GNU (Gówno Nie Używać) and not working well.

--- src/sofin.sh
+++ src/sofin.sh
@@ -452,7 +452,8 @@ if [ ! "$1" = "" ]; then
     l|installed|list)
         debug "Listing installed software alphabetically:"
         if [ -d ${SOFTWARE_DIR} ]; then
-            ${FIND_BIN} ${SOFTWARE_DIR} -maxdepth 1 -mindepth 1 -type d  -not -name ".*" -print 2>/dev/null | ${XARGS_BIN} ${BASENAME_BIN} 2>/dev/null | ${SORT_BIN} 2>/dev/null
+            ${FIND_BIN} ${SOFTWARE_DIR} -maxdepth 1 -mindepth 1 -type d  -not -name ".*" -print 2>/dev/null | \
+            ${SED_BIN} -e 's#/.*/##' 2>/dev/null | ${SORT_BIN} 2>/dev/null
         fi
         exit
         ;;
