865214f822d5e288b279724da5d8061a5c5823b8
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-02-27 15:51:53 +0100

    If Ccache isn't installed, install it.

--- software-rebuilder/build-and-push-all.sh
+++ software-rebuilder/build-and-push-all.sh
@@ -6,6 +6,8 @@ chmod 600 ~/.ssh/id_rsa
 . /var/ServeD-OS/setup-buildhost
 setup_buildhost
 
+test ! -x /Software/Ccache/bin/ccache || s i Ccache
+
 note "Checking remote machine connection (shouldn't take more than a second).."
 ssh sofin@verknowsys.com "uname -a"
 set +e
