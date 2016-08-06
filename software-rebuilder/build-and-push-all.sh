b31b7bcc5729af79ade9444afd34b87338830bc6
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-08-06 03:34:19 +0200

    imp: Move state file to /var, don't remove bundle after deploy (debugging purposes)

--- software-rebuilder/build-and-push-all.sh
+++ software-rebuilder/build-and-push-all.sh
@@ -15,7 +15,7 @@ run "${SSH_BIN} sofin@verknowsys.com uname -a"
 
 set +e
 
-_working_state_file="/tmp/processing-software.list"
+_working_state_file="/var/software.list.processing"
 if [ ! -f "${_working_state_file}" ]; then
     note "Creating new file: $(distn "${_working_state_file}")"
     run "${CP_BIN} -v software.list ${_working_state_file}"
@@ -28,15 +28,16 @@ for software in $(${CAT_BIN} ${_working_state_file} 2>/dev/null); do
     fi
     note "________________________________"
 
-    if [ "FreeBSD" = "${SYSTEM_NAME}" ]; then
+    if [ "FreeBSD" = "${SYSTEM_NAME}" -a \
+         -z "${DEVEL}" ]; then
         ${SOFIN_BIN} reset && \
-            note "Sofin definitions reset for production host type"
+            note "Sofin definitions reset for production host type with undefined $(distn "DEVEL")"
     fi
 
     note "Processing software: $(distn "${software}")"
     ${SOFIN_BIN} rm ${software}
     DEVEL=YES ${SOFIN_BIN} deploy ${software} && \
-    ${SOFIN_BIN} rm ${software} && \
     ${SED_BIN} -i '' -e "/${software}/d" ${_working_state_file}
+    # ${SOFIN_BIN} rm ${software} && \
     note "--------------------------------"
 done
