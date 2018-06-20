5f839262d591ea15f3eef3fcb0539ccced433388
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-06-20 14:23:29 +0200

    new: Imported new Sofin binaries by borrowing great code of "smallz4" - MIT licensed Lz4 utilities replacement.

---
+++ src/s-lz4.cc
@@ -0,0 +1,190 @@
+// //////////////////////////////////////////////////////////
+// smallz4.cpp
+// Copyright (c) 2016 Stephan Brumme. All rights reserved.
+// see http://create.stephan-brumme.com/smallz4/
+//
+// "MIT License":
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software
+// is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "smallz4.h"
+
+#include <stdio.h>    // stdin/stdout/stderr, fopen, ...
+#ifdef _WIN32
+  #include <io.h>     // isatty()
+#else
+  #include <unistd.h> // isatty()
+  #define _fileno fileno
+  #define _isatty isatty
+#endif
+
+
+/// error handler
+static void error(const char* msg)
+{
+  fprintf(stderr, "ERROR: %s\n", msg);
+  exit(1);
+}
+
+
+// ==================== I/O INTERFACE ====================
+
+
+/// input stream,  usually stdin
+FILE* in = 0;
+/// read several bytes and store at "data", return number of actually read bytes (return only zero if end of data reached)
+size_t getBytesFromIn(void* data, size_t numBytes)
+{
+  if (data && numBytes > 0)
+    return fread(data, 1, numBytes, in);
+  return 0;
+}
+
+/// output stream, usually stdout
+FILE* out = 0;
+/// write a block of bytes
+void sendBytesToOut(const void* data, size_t numBytes)
+{
+  if (data && numBytes > 0)
+    fwrite(data, 1, numBytes, out);
+}
+
+
+// ==================== COMMAND-LINE HANDLING ====================
+
+
+// show simple help
+static void showHelp(const char* program)
+{
+  printf("smalLZ4 %s: LZ4 compressor with optimal parsing, fully compatible with LZ4\n"
+    "\n"
+    "Basic usage:\n"
+    "  %s [flags] [input] [output]\n"
+    "\n"
+    "This program writes to STDOUT if output isn't specified\n"
+    "and reads from STDIN if input isn't specified, either.\n"
+    "\n"
+    "Examples:\n"
+    "  %s   < abc.txt > abc.txt.lz4    # use STDIN and STDOUT\n"
+    "  %s     abc.txt > abc.txt.lz4    # read from file and write to STDOUT\n"
+    "  %s     abc.txt   abc.txt.lz4    # read from and write to file\n"
+    "  cat abc.txt | %s - abc.txt.lz4  # read from STDIN and write to file\n"
+    "  %s -6  abc.txt   abc.txt.lz4    # compression level 6 (instead of default 9)\n"
+    "  %s -f  abc.txt   abc.txt.lz4    # overwrite an existing file\n"
+    "  %s -f7 abc.txt   abc.txt.lz4    # compression level 7 and overwrite an existing file\n"
+    "\n"
+    "Flags:\n"
+    "  -0, -1 ... -9   Set compression level, default: 9 (see below)\n"
+    "  -h              Display this help message\n"
+    "  -f              Overwrite an existing file\n"
+    "\n"
+    "Compression levels:\n"
+    " -0               No compression\n"
+    " -1 ... -%d        Greedy search, check 1 to %d matches\n"
+    " -%d ... -8        Lazy matching with optimal parsing, check %d to 8 matches\n"
+    " -9               Optimal parsing, check all possible matches\n"
+    "\n"
+    "(C) 2016 Stephan Brumme, http://create.stephan-brumme.com/smallz4/\n"
+    , smallz4::getVersion()
+    , program, program, program, program, program, program, program, program,
+    smallz4::ShortChainsGreedy,     smallz4::ShortChainsGreedy,
+    smallz4::ShortChainsGreedy + 1, smallz4::ShortChainsGreedy + 1);
+}
+
+
+/// parse command-line
+int main(int argc, const char* argv[])
+{
+  // show help if no parameters and stdin isn't a pipe
+  if (argc == 1 && _isatty(_fileno(stdin)) != 0)
+  {
+    showHelp(argv[0]);
+    return 0;
+  }
+
+  unsigned int maxChainLength = 65536; // "unlimited" because search window contains only 2^16 bytes
+
+  // overwrite output ?
+  bool overwrite = false;
+
+  // parse flags
+  int nextArgument = 1;
+  while (argc > nextArgument && argv[nextArgument][0] == '-')
+  {
+    int argPos = 1;
+    while (argv[nextArgument][argPos] != '\0')
+    {
+      switch (argv[nextArgument][argPos++])
+      {
+      // show help
+      case 'h':
+        showHelp(argv[0]);
+        return 0;
+
+      // force overwrite
+      case 'f':
+        overwrite = true;
+        break;
+
+      // set compression level
+      case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8':
+        maxChainLength = argv[nextArgument][1] - '0'; // "0" => 0, "1" => 1, ..., "8" => 8
+        break;
+
+      // unlimited hash chain length
+      case '9':
+        // default maxChainLength is already "unlimited"
+        break;
+
+      default:
+        error("unknown flag");
+      }
+    }
+
+    nextArgument++;
+  }
+
+  // default input/output streams
+  in = stdin; out = stdout;
+
+  // input file is given as first parameter or stdin if no parameter is given (or "-")
+  if (argc > nextArgument && argv[nextArgument][0] != '-')
+  {
+    in = fopen(argv[nextArgument], "rb");
+    if (!in)
+      error("file not found");
+    nextArgument++;
+  }
+
+  // output file is given as second parameter or stdout if no parameter is given (or "-")
+  if (argc == nextArgument + 1 && argv[nextArgument][0] != '-')
+  {
+    // check if file already exists
+    if (!overwrite && fopen(argv[nextArgument], "rb"))
+      error("output file already exists");
+
+    out = fopen(argv[nextArgument], "wb");
+    if (!out)
+      error("cannot create file");
+  }
+
+  // and go !
+  smallz4::lz4(getBytesFromIn, sendBytesToOut, maxChainLength);
+  return 0;
+}
---
+++ src/s-lz4cat.cc
@@ -0,0 +1,319 @@
+// //////////////////////////////////////////////////////////
+// smallz4cat.c
+// Copyright (c) 2016 Stephan Brumme. All rights reserved.
+// see http://create.stephan-brumme.com/smallz4/
+//
+// "MIT License":
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software
+// is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// This program is a shorter, more readable, albeit slower re-implementation of lz4cat ( https://github.com/Cyan4973/xxHash )
+
+// Limitations:
+// - skippable frames and legacy frames are not implemented (and most likely never will)
+// - checksums are not verified (see http://create.stephan-brumme.com/xxhash/ for a simple implementation)
+
+// Replace getByteFromIn() and sendToOut() by your own code if you need in-memory LZ4 decompression.
+// Corrupted data causes a call to error().
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdint.h> // uint32_t
+#include <stdio.h>  // stdin/stdout/stderr, fopen, ...
+#include <stdlib.h> // exit()
+#include <string.h> // memcpy
+
+
+/// error handler
+void error(const char* msg)
+{
+  fprintf(stderr, "ERROR: %s\n", msg);
+  exit(1);
+}
+
+
+// ==================== I/O INTERFACE ====================
+
+
+// read one byte from input, see getByteFromIn()  for a basic implementation
+typedef unsigned char (*GET_BYTE)  ();
+// write several bytes,      see sendBytesToOut() for a basic implementation
+typedef void          (*SEND_BYTES)(const unsigned char*, unsigned int);
+
+/// input stream,  usually stdin
+static FILE* in = NULL;
+/// read a single byte (with simple buffering)
+static unsigned char getByteFromIn()
+{
+  // modify buffer size as you like ... for most use cases, bigger buffer aren't faster anymore - and even reducing to 1 byte works !
+#define READ_BUFFER_SIZE 4*1024
+  static unsigned char readBuffer[READ_BUFFER_SIZE];
+  static unsigned int  pos       = 0;
+  static unsigned int  available = 0;
+
+  // refill buffer
+  if (pos == available)
+  {
+    pos = 0;
+    available = fread(readBuffer, 1, READ_BUFFER_SIZE, in);
+    if (available == 0)
+      error("out of data");
+  }
+
+  // return a byte
+  return readBuffer[pos++];
+}
+
+/// output stream, usually stdout
+static FILE* out = NULL;
+/// write a block of bytes
+static void sendBytesToOut(const unsigned char* data, unsigned int numBytes)
+{
+  if (data != NULL && numBytes > 0)
+    fwrite(data, 1, numBytes, out);
+}
+
+
+// ==================== LZ4 DECOMPRESSOR ====================
+
+
+/// decompress everything in input stream (accessed via getByte) and write to output stream (via sendBytes)
+void unlz4(GET_BYTE getByte, SEND_BYTES sendBytes)
+{
+  // signature
+  unsigned char signature1 = getByte();
+  unsigned char signature2 = getByte();
+  unsigned char signature3 = getByte();
+  unsigned char signature4 = getByte();
+  uint32_t signature = (signature4 << 24) | (signature3 << 16) | (signature2 << 8) | signature1;
+  int isModern = (signature == 0x184D2204);
+  int isLegacy = (signature == 0x184C2102);
+  if (!isModern && !isLegacy)
+    error("invalid signature");
+
+  unsigned char hasBlockChecksum   = 0;
+  unsigned char hasContentSize     = 0;
+  unsigned char hasContentChecksum = 0;
+  if (isModern)
+  {
+    // flags (version is ignored)
+    unsigned char flags = getByte();
+    hasBlockChecksum   = flags & 16;
+    hasContentSize     = flags &  8;
+    hasContentChecksum = flags &  4;
+
+    // ignore blocksize
+    getByte();
+
+    if (hasContentSize)
+    {
+      // ignore, skip 8 bytes
+      getByte(); getByte(); getByte(); getByte();
+      getByte(); getByte(); getByte(); getByte();
+    }
+
+    // ignore header checksum (xxhash32 of everything up this point & 0xFF)
+    getByte();
+  }
+
+  // don't lower this value, backreferences can be 64kb far away
+#define HISTORY_SIZE 64*1024
+  // contains the latest decoded data
+  unsigned char history[HISTORY_SIZE];
+  // next free position in history[]
+  unsigned int  pos = 0;
+
+  // parse all blocks until blockSize == 0
+  while (1)
+  {
+    // block size
+    uint32_t blockSize = getByte();
+    blockSize |= (uint32_t)getByte() <<  8;
+    blockSize |= (uint32_t)getByte() << 16;
+    blockSize |= (uint32_t)getByte() << 24;
+
+    // highest bit set ?
+    unsigned char isCompressed = isLegacy || (blockSize & 0x80000000) == 0;
+    if (isModern)
+      blockSize &= 0x7FFFFFFF;
+
+    // stop after last block
+    if (blockSize == 0)
+      break;
+
+    if (isCompressed)
+    {
+      // decompress block
+      uint32_t blockOffset = 0;
+      while (blockOffset < blockSize)
+      {
+        // get a token
+        unsigned char token = getByte();
+
+        blockOffset++;
+
+        // determine number of literals
+        uint32_t numLiterals = (token >> 4) & 0x0F;
+        if (numLiterals == 15)
+        {
+          // number of literals length encoded in more than 1 byte
+          unsigned char current;
+          do
+          {
+            current = getByte();
+            numLiterals += current;
+            blockOffset++;
+          } while (current == 255);
+        }
+
+        blockOffset += numLiterals;
+        // copy all those literals
+        while (numLiterals-- > 0)
+        {
+          history[pos++] = getByte();
+
+          // flush output buffer
+          if (pos == HISTORY_SIZE)
+          {
+            sendBytes(history, HISTORY_SIZE);
+            pos = 0;
+          }
+        }
+
+        // last token has only literals
+        if (blockOffset == blockSize)
+          break;
+
+        // match distance is encoded by two bytes (little endian)
+        blockOffset += 2;
+        uint32_t delta = getByte();
+        delta |= (uint32_t)getByte() << 8;
+        // zero isn't allowed
+        if (delta == 0)
+          error("invalid offset");
+
+        // match length (must be >= 4, therefore length is stored minus 4)
+        uint32_t matchLength = 4 + (token & 0x0F);
+        if (matchLength == 4 + 0x0F)
+        {
+          unsigned char current;
+          do // match length encoded in more than 1 byte
+          {
+            current = getByte();
+            matchLength += current;
+            blockOffset++;
+          } while (current == 255);
+        }
+
+        // copy match
+        uint32_t reference = (pos >= delta) ? pos - delta : HISTORY_SIZE + pos - delta;
+        if (pos + matchLength < HISTORY_SIZE && reference + matchLength < HISTORY_SIZE)
+        {
+          // fast copy
+          if (pos >= reference + matchLength || reference >= pos + matchLength)
+          {
+            // non-overlapping
+            memcpy(history + pos, history + reference, matchLength);
+            pos += matchLength;
+          }
+          else
+          {
+            // overlapping
+            while (matchLength-- > 0)
+              history[pos++] = history[reference++];
+          }
+        }
+        else
+        {
+          // slower copy, have to take care of buffer limits
+          while (matchLength-- > 0)
+          {
+            // copy single byte
+            history[pos++] = history[reference++];
+
+            // cannot write anymore ? => wrap around
+            if (pos == HISTORY_SIZE)
+            {
+              // flush output buffer
+              sendBytes(history, HISTORY_SIZE);
+              pos = 0;
+            }
+            // cannot read anymore ? => wrap around
+            if (reference == HISTORY_SIZE)
+              reference = 0;
+          }
+        }
+      }
+
+      // all legacy blocks must be completely filled - except for the last one
+      if (isLegacy && blockSize < 8*1024*1024)
+        break;
+    }
+    else
+    {
+      // copy uncompressd data and add to history, too (if next block is compressed and some matches refer to this block)
+      while (blockSize-- > 0)
+      {
+        // copy a byte ...
+        history[pos++] = getByte();
+        // ... until buffer is full => send to output
+        if (pos == HISTORY_SIZE)
+        {
+          sendBytes(history, HISTORY_SIZE);
+          pos = 0;
+        }
+      }
+    }
+
+    if (hasBlockChecksum)
+    {
+      // ignore checksum, skip 4 bytes
+      getByte(); getByte(); getByte(); getByte();
+    }
+  }
+
+  if (hasContentChecksum)
+  {
+    // ignore checksum, skip 4 bytes
+    getByte(); getByte(); getByte(); getByte();
+  }
+
+  // flush output buffer
+  sendBytes(history, pos);
+}
+
+
+// ==================== COMMAND-LINE HANDLING ====================
+
+
+/// parse command-line
+int main(int argc, const char* argv[])
+{
+  // default input/output streams
+  in = stdin; out = stdout;
+
+  // first command-line parameter is our input filename / but ignore "-" which stands for STDIN
+  if (argc == 2 && argv[1][0] != '-' && argv[1][1] != '\0')
+  {
+    in = fopen(argv[1], "rb");
+    if (!in)
+      error("file not found");
+  }
+
+  // and go !
+  unlz4(getByteFromIn, sendBytesToOut);
+  return 0;
+}
