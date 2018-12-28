cf109046969a42ea9e04c7e530843b2a822b2da1
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-12-28 19:34:50 +0100

    imp: Better build-and-push-all script. More in details.
    * Defined wipeout of all possibly existing datasets, servicedirs and cached files - it's DESTRUCTIVE! Be warned to not use it on production!!
    * Perform two wipeouts - before and after each bundle.
    * Use utilities for production build :)
    * Follow variables naming convention
    * Switched note() to permnote()

--- software-rebuilder/build-and-push-all.sh
+++ software-rebuilder/build-and-push-all.sh
@@ -11,8 +11,7 @@ SOFIN_ROOT="${SOFIN_ROOT:-/Software/Sofin}"
 # fi
 
 # ${TEST_BIN} -f /.build-host && export DEVEL=YES
-${TEST_BIN} ! -x /Software/Ccache/bin/ccache || ${SOFIN_BIN} i Ccache
-
+# ${TEST_BIN} ! -x /Software/Ccache/bin/ccache || ${SOFIN_BIN} i Ccache
 # USE_NO_UTILS=YES s d Pkgconf Make Cmake Bison Zip
 
 note "Checking remote machine connection (shouldn't take more than a second).."
@@ -22,35 +21,43 @@ set +e
 
 unset USE_NO_TEST
 
-_working_state_file="/var/software.list.processing"
+_working_state_file="/tmp/software.list.processing"
 if [ ! -f "${_working_state_file}" ]; then
     permnote "Creating new work-state file: $(distn "${_working_state_file}")"
     run "${CP_BIN} -v software.list ${_working_state_file}"
 fi
 
-for software in $(${CAT_BIN} ${_working_state_file} 2>/dev/null); do
-    if [ "${software}" = "------" ]; then
-        note "All tasks finished!"
+for _software in $(${CAT_BIN} ${_working_state_file} 2>/dev/null); do
+    if [ "${_software}" = "------" ]; then
+        permnote "All tasks finished!"
         exit
     fi
     permnote "________________________________"
 
-    _indicator="/Software/${software}/$(lowercase "${software}")${DEFAULT_INST_MARK_EXT}"
-    if [ -d "/Software/${software}" ] && [ -f "${_indicator}" ]; then
-        warn "Found already prebuilt version of software: $(distw "${software}"). Leaving untouched with version: $(distw "$(${CAT_BIN} "${_indicator}" 2>/dev/null)")"
-        ${SED_BIN} -i '' -e "/${software}/d" ${_working_state_file}
+    _indicator="/Software/${_software}/$(lowercase "${_software}")${DEFAULT_INST_MARK_EXT}"
+    if [ -d "/Software/${_software}" ] && [ -f "${_indicator}" ]; then
+        warn "Found already prebuilt version of software: $(distw "${_software}"). Leaving untouched with version: $(distw "$(${CAT_BIN} "${_indicator}" 2>/dev/null)")"
+        ${SED_BIN} -i '' -e "/${_software}/d" ${_working_state_file}
     else
+        destroy_software_and_datadirs () {
+            try "${SOFIN_BIN} rm ${_software}"
+            try "${ZFS_BIN} destroy -fr ${DEFAULT_ZPOOL}${SOFTWARE_DIR}/root/${_software}"
+            try "${ZFS_BIN} destroy -fr ${DEFAULT_ZPOOL}${SERVICES_DIR}/root/${_software}"
+            try "${RM_BIN} -rf ${HOME}/.sofin/file-cache/$(capitalize "${_software}")* '${SOFTWARE_DIR}/${_software}' '${SERVICES_DIR}/${_software}'"
+        }
         remove_from_list_and_destroy () {
-            try "${SED_BIN} -i '' -e \"/${software}/d\" ${_working_state_file}"
-            try "${SOFIN_BIN} rm ${software}"
+            try "${SED_BIN} -i '' -e \"/${_software}/d\" ${_working_state_file}"
+            destroy_software_and_datadirs
         }
 
-        note "Removing software: $(distn "${software}")"
-        try "${SOFIN_BIN} rm ${software}"
+        permnote "Removing software: $(distn "${_software}")"
+        try "${SOFIN_BIN} rm ${_software}"
 
-        note "Deploying software: $(distn "${software}")"
-        USE_NO_UTILS=YES ${SOFIN_BIN} deploy ${software} \
-            && remove_from_list_and_destroy
+        permnote "Deploying software: $(distn "${_software}")"
+        destroy_software_and_datadirs
+        # USE_NO_UTILS=YES
+        ${SOFIN_BIN} deploy ${_software}
+        remove_from_list_and_destroy
 
     fi
     permnote "--------------------------------"
